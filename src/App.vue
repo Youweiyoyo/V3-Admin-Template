04b1b13333258be769cd7c2906c80b0d96136f62
Youwei <yoyoyo9527@163.com>
2022-06-20 22:10:59 +0800

    project init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useUserStore } from '@/store/user/user'
+const userStore = useUserStore()
+const changeName = () => {
+  userStore.updateName('王五')
+}
+</script>
+
+<template>
+  <div>{{userStore.name}}</div>
+  <button @click="changeName">修改store</button>
+  <RouterView/>
+</template>
+
+<style>
+</style>
---
+++ src/view/Home/home.vue
@@ -0,0 +1,12 @@
+<template>
+<div>
+  home组件
+</div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+
+</style>
